4b04192e8aa097242fc3ee6e4b74ac75624ba90e
Alvin <alvin.chan@elasticpath.com>
2020-05-21 11:27:51 -0400

    Change folders that are created and add resizing abilities

--- Scripts/ConvertFiles.sh
+++ Scripts/ConvertFiles.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+mkdir ./Images/converted
 # Create the folder to store Next Gen images
-mkdir ./Images/JP2Files
-mkdir ./Images/WebpFiles
+mkdir ./Images/converted/jp2
+mkdir ./Images/converted/webp
 # mkdir ./Images/JXRFiles
-mkdir ./Images/Placeholders
-mkdir ./Images/jpg
+mkdir ./Images/converted/Placeholders
+mkdir ./Images/converted/jpg
 
 # Go into Image directory for easier understanding
 cd Images
@@ -23,16 +24,31 @@ for file in *; do
       # -strip gets rid unnecessary metadata
       # -quality 1 - 100, specifies image quality
       # -resize creates thumbnail like images 4096@ = 64x64 16384@ 128x128
-      convert $file -strip -quality 1 -colors 255 -resize 4096@ ./Placeholders/$fileName.png
+      convert $file -strip -quality 1 -colors 255 -resize 4096@ ./converted/Placeholders/$fileName.png
     else
-      convert $file -strip -quality 20 -resize 16384@ ./png/$fileName.jpg
+      convert $file -strip -quality 20 -resize 16384@ ./converted/png/$fileName.jpg
     fi
 
+    # TODO: Need to make images smaller too...
+
     # Conversion to Next Gen formats, using solely imageMagick defaults
-    convert $file -quality 100 ./WebpFiles/$fileName.webp
-    convert $file ./JP2Files/$fileName.jp2
-    # convert $file ./JXRFiles/$fileName.jxr
-    convert $file ./jpg/$fileName.jpg
+
+    ## We need to downsize every single file...
+
+    # resize and convert to webp
+    convert $file -quality 100 -resize 620x620 ./converted/webp/$fileName-768w.webp
+    convert $file -quality 100 -resize 490x490 ./converted/webp/$fileName-1092w.webp
+    convert $file -quality 100 -resize 450x450 ./converted/webp/$fileName-2800w.webp
+
+    # resize and convert to jp2
+    convert $file -resize 620x620 ./converted/jp2/$fileName-768w.jp2
+    convert $file -resize 490x490 ./converted/jp2/$fileName-1092w.jp2
+    convert $file -resize 450x450 ./converted/jp2/$fileName-2800w.jp2
+
+    # resize and convert $file to jpg
+    convert $file -resize 620x620 ./converted/jpg/$fileName-768w.jpg
+    convert $file -resize 490x490 ./converted/jpg/$fileName-1092w.jpg
+    convert $file -resize 450x450 ./converted/jpg/$fileName-2800w.jpg
 
   fi
 
